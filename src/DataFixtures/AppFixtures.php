70f24949e6443874b838598b01d1c8366cb57469
adjal karim <adjal.karim@live.fr>
2023-09-14 13:09:39 +0200

    ajout fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,7 +6,8 @@ use App\Entity\Categorie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Chambre;
-use App\Repository\CategorieRepository;
+use App\Entity\Reservation;
+use App\Entity\User;
 use Faker\Factory;
 
 
@@ -55,11 +56,58 @@ for ($j = 1; $j <= 10; $j++) {
     $manager->persist($chambre);
 
 
-    $manager->persist($chambre);
+$users = [];
+
+for ($i = 0; $i < 10; $i++) {
+    $user = new User();
+
+    $user->setNom($faker->lastName);
+    $user->setPrenom($faker->firstName);
+    $user->setEmail($faker->email);
+    $user->setAdresse($faker->word());
+    $user->setDateDeNaissance($faker->dateTime());
+    $user->setTelephone($faker->phoneNumber);
+    $user->setPassword($faker->password);
+
+    $manager->persist($user);
+
+    $users[] = $user;
+}
+
+//Proleme date anterieur
+
+for ($j = 0; $j <= 10; $j++) {
+    $reservation = new Reservation();
+
+    $dateReservation = $faker->dateTimeBetween();
+    $dateEntrée = $faker->dateTimeBetween($dateReservation, '+1 month');
+    $dateSortie = $faker->dateTimeBetween($dateEntrée, '+1 week');
+
+    $reservation->setDateReservation($dateReservation);
+    $reservation->setDateEntree($dateEntrée);
+    $reservation->setDateSortie($dateSortie);
+
+     // Associer la réservation à un utilisateur aléatoir
+
+    $userIndex = array_rand($users);
+    $user = $users[$userIndex];
+    $reservation->setUser($user);
+
+    $manager->persist($reservation);
+}
+
+
+$manager->flush();
+
+
+
+
+}
+
+ 
    
 
-            $manager->flush();
         }
    
     }
-}
+
