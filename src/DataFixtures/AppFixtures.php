f9716a42ad125d3cc4b071ca9cabc08c40d531ad
adjal karim <adjal.karim@live.fr>
2023-09-15 13:55:58 +0200

    modification fixtures et ajout css chambre

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,113 +1,111 @@
 <?php
 
+
 namespace App\DataFixtures;
 
-use App\Entity\Categorie;
+use App\Entity\User;
+use App\Entity\Reservation;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Chambre;
-use App\Entity\Reservation;
-use App\Entity\User;
+use App\Entity\Categorie;
 use Faker\Factory;
+use App\Entity\ReservationChambre;
 
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create('fr_FR');
+        $faker = Factory::create();
 
-        $categories = []; 
+        $categories = [];
 
-for ($i = 0; $i < 5; $i++) {
-    $categorie = new Categorie();
-    $categorie->setChambreDoubleSuperieure($faker->word);
-    $categorie->setChambreDoubleDeluxe($faker->word); 
-    $categorie->setSuiteJunior($faker->word); 
+        for ($i = 0; $i < 50; $i++) {
+            $categorie = new Categorie();
+            $categorie->setChambreDoubleSuperieure($faker->word);
+            $categorie->setChambreDoubleDeluxe($faker->word);
+            $categorie->setSuiteJunior($faker->word);
 
-    $manager->persist($categorie);
+            $manager->persist($categorie);
 
-
-    $categories[] = $categorie;
-}
-
-
-
-        $chambres  = [];
-
-for ($j = 1; $j <= 10; $j++) {
-    $chambre = new Chambre();
-    $chambre->setChainesSatellite($faker->boolean(true, false));
-    $chambre->setTelephone($faker->boolean(true, false));
-    $chambre->setTelevisionàEcranPlat($faker->boolean(true, false));
-    $chambre->setTarif($faker->numberBetween(50, 200));
-    $chambre->setSuperficie($faker->numberBetween(40, 100));
-    $chambre->setVueSurMer($faker->boolean(true, false));
-    $chambre->setChainesàLaCarte($faker->boolean(true,false));
-    $chambre->setClimatisation($faker->boolean(true,false));
-    $chambre->setChainesDuCable($faker->boolean(true,false));
-    $chambre->setCoffreFort($faker->boolean(true,false));
-    $chambre->setMaterielDeRepassage($faker->boolean(true,false));
-    $chambre->setWifiGratuit($faker->boolean(true,false));
-    $categorie = $faker->randomElement($categories); 
-
-    $chambre->setCategorie($categorie);
-
-    $manager->persist($chambre);
-
-
-$users = [];
-
-for ($i = 0; $i < 10; $i++) {
-    $user = new User();
-
-    $user->setNom($faker->lastName);
-    $user->setPrenom($faker->firstName);
-    $user->setEmail($faker->email);
-    $user->setAdresse($faker->word());
-    $user->setDateDeNaissance($faker->dateTime());
-    $user->setTelephone($faker->phoneNumber);
-    $user->setPassword($faker->password);
-
-    $manager->persist($user);
-
-    $users[] = $user;
-}
-
-//Proleme date anterieur
-
-for ($j = 0; $j <= 10; $j++) {
-    $reservation = new Reservation();
-
-    $dateReservation = $faker->dateTimeBetween();
-    $dateEntrée = $faker->dateTimeBetween($dateReservation, '+1 month');
-    $dateSortie = $faker->dateTimeBetween($dateEntrée, '+1 week');
-
-    $reservation->setDateReservation($dateReservation);
-    $reservation->setDateEntree($dateEntrée);
-    $reservation->setDateSortie($dateSortie);
-
-     // Associer la réservation à un utilisateur aléatoir
-
-    $userIndex = array_rand($users);
-    $user = $users[$userIndex];
-    $reservation->setUser($user);
-
-    $manager->persist($reservation);
-}
-
-
-$manager->flush();
-
-
-
-
-}
-
- 
-   
-
+            $categories[] = $categorie;
         }
-   
-    }
 
+        $chambres = [];
+
+        for ($j = 0; $j <= 10; $j++) {
+            $chambre = new Chambre();
+            $chambre->setTarif($faker->numberBetween(100, 500));
+            $chambre->setSuperficie($faker->numberBetween(16, 50) . ' m²');
+            $chambre->setVueSurMer($faker->boolean);
+            $chambre->setChainesàLaCarte($faker->boolean);
+            $chambre->setClimatisation($faker->boolean);
+            $chambre->setTelevisionàEcranPlat($faker->boolean);
+            $chambre->setTelephone($faker->boolean);
+            $chambre->setChainesSatellite($faker->boolean);
+            $chambre->setChainesDuCable($faker->boolean);
+            $chambre->setCoffreFort($faker->boolean);
+            $chambre->setMaterielDeRepassage($faker->boolean);
+            $chambre->setWiFiGratuit($faker->boolean);
+
+            $categorie = $faker->randomElement($categories);
+
+            $chambre->setCategorie($categorie);
+
+            $manager->persist($chambre);
+
+            $chambres[] = $chambre;
+        }
+
+        $users = [];
+
+        for ($i = 0; $i < 50; $i++) {
+            $user = new User();
+
+            $user->setNom($faker->lastName);
+            $user->setPrenom($faker->firstName);
+            $user->setEmail($faker->email);
+            $user->setAdresse($faker->address);
+            $user->setDateDeNaissance($faker->dateTimeBetween('-50 years', '-18 years'));
+            $user->setTelephone($faker->phoneNumber);
+            $user->setPassword($faker->password);
+
+            $manager->persist($user);
+
+            $users[] = $user;
+        }
+
+        for ($j = 0; $j <= 50; $j++) {
+            $reservation = new Reservation();
+
+            $dateReservation = $faker->dateTimeBetween('-1 year', 'now');
+            $dateSortie = $faker->dateTimeBetween($dateReservation, '+1 month');
+            $dateEntrée = $faker->dateTimeBetween('-1 year', $dateSortie);
+
+            $user = $faker->randomElement($users);
+            $reservation->setUser($user);
+
+            $reservation->setDateReservation($dateReservation);
+            $reservation->setDateEntree($dateEntrée);
+            $reservation->setDateSortie($dateSortie);
+
+            $manager->persist($reservation);
+
+            // Associer des chambres à la réservation en utilisant l'entité pivot
+            $chambresReservees = $faker->randomElements($chambres, $faker->numberBetween(1, 5));
+
+            foreach ($chambresReservees as $chambre) {
+                $reservationChambre = new ReservationChambre();
+                $reservationChambre->setChambre($chambre);
+                $reservationChambre->setReservation($reservation);
+                $reservationChambre->setDateReservation($dateReservation);
+
+
+                $manager->persist($reservationChambre);
+            }
+        }
+
+        $manager->flush();
+    }
+}
