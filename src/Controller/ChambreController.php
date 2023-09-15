feff349d38575e903c2dc44c404861acb5d6e89e
axsxpro <cmptpro@outlook.com>
2023-09-15 17:40:21 +0200

    mis à jour

---
+++ src/Controller/ChambreController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ChambreController extends AbstractController
+{
+    #[Route('/chambre', name: 'app_chambre')]
+    public function index(): Response
+    {
+        return $this->render('chambre/index.html.twig', [
+            'controller_name' => 'ChambreController',
+        ]);
+    }
+
+
+    #[Route('/chambre/junior', name: 'app_chambre_junior')]
+    public function junior(): Response
+    {
+        return $this->render('chambre/chambreJunior.html.twig', [
+            'controller_name' => 'ChambreController',
+        ]);
+    }
+}
+
